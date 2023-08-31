11d6f46276d823c6d10a7178bcfa295cbd9b9544
Nikolai <borodinov.nikolai@gmail.com>
2023-08-31 20:29:57 +0300

    commit

--- next/src/app/api/auth/route.ts
+++ next/src/app/api/auth/route.ts
@@ -1,4 +1,4 @@
-import { $axios } from '@/http'
+import { $axios } from '@/axios'
 
 
 export const fetchCache = 'only-no-store'
--- next/src/services/getProducts.ts
+++ next/src/services/getProducts.ts
@@ -1,4 +1,4 @@
-import { $axios } from '@/http'
+import { $axios } from '@/axios'
 
 
 
