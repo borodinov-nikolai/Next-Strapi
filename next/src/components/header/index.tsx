89501541be8cf4e61dedce65e1ef2fe3ce196373
borodinov_nikolai <borodinov.nikolai@gmail.com>
2023-10-09 21:56:20 +0300

    commit

--- next/src/components/header/index.tsx
+++ next/src/components/header/index.tsx
@@ -7,67 +7,78 @@ import {Html5Outlined} from '@ant-design/icons'
 
 const Header = ({ user }: { user: any }) => {
   return (
-    <div className="container">
-      <div className={styles.root}>
-        
-       
-        <nav className={styles.navbar}>
-
-
-          <ul className={styles.leftBlock}>
-          <li  className={styles.logo}>
-              <Link href="/">
-              <Html5Outlined style={{fontSize: '28px'}} /> 
-              <div style={{fontSize: '20px', fontFamily:'cursive'}} >Техно-Маркет</div>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <div>Главная</div>
-              </Link>
-            </li>
-            <li className={styles.catalog} >
-              <Link href="/catalog/smartphones">
-                <div>Каталог</div>
-              </Link>
-             <ul className={styles.catalog_submenu} >
-              <li>Смартфоны</li>
-              <li>Планшеты</li>
-             </ul>
-            </li>
-            <li>
-              <Link href="/contacts">
-                <div>Конаткты</div>
-              </Link>
-            </li>
-            <li>
-              <Link href="/about">
-                <div>О нас</div>
-              </Link>
-            </li>
-          </ul>
-       
-
-        <ul className={styles.rightBlock}>
-          <li><Cart user={user} /></li>
-          <li> <Auth user={user} /></li>
-          
-         
-        </ul>
-
+   
+      <div className="container">
+        <div className={styles.root}>
       
+      
+          <nav className={styles.navbar}>
+            <ul className={styles.leftBlock}>
+            <li  className={styles.logo}>
+                <Link href="/">
+                <Html5Outlined style={{fontSize: '28px'}} />
+                <div style={{fontSize: '20px', fontFamily:'cursive'}} >Техно-Маркет</div>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <div>Главная</div>
+                </Link>
+              </li>
+              <li className={styles.catalog} >
+                <Link href="/catalog/smartphones">
+                  <div>Каталог</div>
+                </Link>
+              
+         
+                   <div className={styles.submenu}>
+                     <ul className={styles.submenu_list} >
+                      <li>Смартфоны</li>
+                      <li>Планшеты</li>
+                      <li>Ноутбуки</li>
+                      <li>Телевизоры</li>
+                      <li>Игровые Консоли</li>
+                     
+                     </ul>
+                   </div>
+               
+                     {/* <div className={styles.fade}></div> */}
 
-        </nav>
+              </li>
+              <li>
+                <Link href="/contacts">
+                  <div>Конаткты</div>
+                </Link>
+              </li>
+              <li>
+                <Link href="/about">
+                  <div>О нас</div>
+                </Link>
+              </li>
+            </ul>
+      
+          <ul className={styles.rightBlock}>
+            <li><Cart user={user} /></li>
+            <li> <Auth user={user} /></li>
+      
+      
+          </ul>
+      
+          </nav>
+          <div className={styles.mobileNavbar}>
+            <MobileNavbar
+              user={user}
+              cart={<Cart user={user} />}
+            />
+          </div>
 
-        <div className={styles.mobileNavbar}>
-          <MobileNavbar
-            user={user}
-            cart={<Cart user={user} />}
-          />
+        
+
         </div>
-
+        
       </div>
-    </div>
+  
+  
   );
 };
 
