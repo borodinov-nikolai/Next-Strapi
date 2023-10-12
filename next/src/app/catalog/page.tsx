188beaa156607b96dce638129b6f3b5cc34346a9
Nikolai <borodinov.nikolai@gmail.com>
2023-10-12 15:25:51 +0300

    commit

--- next/src/app/catalog/page.tsx
+++ next/src/app/catalog/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import styles from './catalog.module.scss'
+
 
 const Catalog = () => {
   return (
-    <div>Catalog</div>
+      <div className={styles.root} >
+    <div className='container'>
+     <div className={styles.menu} >
+        <div className={styles.menu_item}></div>
+        <div className={styles.menu_item}></div>
+        <div className={styles.menu_item}></div>
+        <div className={styles.menu_item}></div>
+        <div className={styles.menu_item}></div>
+        <div className={styles.menu_item}></div>
+     </div>
+      </div>
+    </div>
   )
 }
 
