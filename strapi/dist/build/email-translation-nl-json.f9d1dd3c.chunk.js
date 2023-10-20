ac90595c6456cac0537585b148f97ea218916582
borodinov-nikolai <127582862+borodinov-nikolai@users.noreply.github.com>
2023-10-20 22:13:48 +0300

    Delete strapi/dist directory

--- strapi/dist/build/email-translation-nl-json.f9d1dd3c.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkstrapi=self.webpackChunkstrapi||[]).push([[3206],{99291:s=>{s.exports={}}}]);
--- strapi/dist/config/api.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    rest: {
-        defaultLimit: 12,
-        maxLimit: 100,
-        withCount: true,
-    },
-};
