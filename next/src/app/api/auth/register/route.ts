5e1c8d3c4441b8b49f4e954d9a69dd539bf82b53
Nikolai <borodinov.nikolai@gmail.com>
2023-10-25 13:04:46 +0300

    commit

--- next/src/app/api/auth/register/route.ts
+++ next/src/app/api/auth/register/route.ts
@@ -23,7 +23,7 @@ export async function POST(request : Request) {
             password
         })
 
-
+         
          
     
     
--- strapi/config/api.ts
+++ strapi/config/api.ts
@@ -1,5 +1,6 @@
 export default {
   rest: {
+    prefix: '/example',
     defaultLimit: 12,
     maxLimit: 100,
     withCount: true,
